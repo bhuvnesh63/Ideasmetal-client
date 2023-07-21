4074820d5374e996dade47ba9cbdae0272788fb3
Bhuvnesh Saini <97789468+bhuvnesh63@users.noreply.github.com>
2023-07-21 17:32:59 +0530

    singleProduct

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,12 +5,12 @@ import "./products.css"
 
 // import Product from './Product/Product'
 
-const Products = ({ products, innerPage, headingText }) => {
-    console.log(products, "deep")
+const Products = ({ items, innerPage, headingText }) => {
+    // console.log(products, "deep")
     return <><div className="products-container">
         {!innerPage && <div className="sec-heading">{headingText}</div>}
         <div className="products">
-            {products && products?.items?.map(item => (
+            {items && items?.map(item => (
                 <Product key={item.id} id={item._id} data={item} />
 
             ))}
